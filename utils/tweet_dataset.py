ecd0a2577f7218d4015e2960d141c7d4a31daa1b
Ben <ane.benoit@gmail.com>
2020-04-09 17:09:30 +0200

    prepa donnees CNN

--- utils/tweet_dataset.py
+++ utils/tweet_dataset.py
@@ -80,3 +80,31 @@ class TweetDataset(Dataset):
 
 
         return idxs,label
+
+		
+		
+class Prepa_CNN(Dataset):
+
+    def __init__(self,df, maxlen, n_most_common_words, batch_size):
+
+        self.df = df
+        self.n_most_common_words = n_most_common_words
+        self.tokenizer = Tokenizer(self.n_most_common_words, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
+        self.maxlen = maxlen
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        
+        sentence = self.df.loc[index, 'tweet']
+        label = self.df.loc[index, 'label']
+        self.tokenizer.fit_on_texts(sentence)
+        sequences = self.tokenizer.texts_to_sequences(sentence)
+        word_index = self.tokenizer.word_index
+        X = pad_sequences(sequences, maxlen= self.maxlen)
+        data = TensorDataset(torch.from_numpy(X), torch.from_numpy(label))
+        loader = DataLoader(data, shuffle=True, batch_size = self.batch_size, drop_last=True)
+
+        return loader
